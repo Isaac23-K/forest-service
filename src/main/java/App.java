59dc67f3a9bfdc00ed3784dbe091b699fff292e0
Isaac23-K <ikaptoge@yahoo.com>
2021-06-15 14:51:27 +0300

    added app.java code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,59 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static spark.Spark.*;
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+        //get sightings
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String,Object>();
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/success", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            model.put("animals",animals);
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //String name, String endangered, String health , String age
+        post("/show", (request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            String name = request.queryParams("rangerName");
+            String endangered = request.queryParams("animal");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            Animal animal = new Animal(name, endangered, health, age);
+            animal.save();
+            model.put("animals",animals);
+//            Sighting newSighting = new Sighting(ranger, location, newAnimal.getId());
+//            model.put("ranger", newSighting.getRanger());
+//            model.put("location",newSighting.getLocation());
+//            newSighting.save();
+//            model.put("animals",Animal.all());
+//            model.put("sighting",Sighting.all());
+//            model.put("AnimalClass",Animal.class);
+//            System.out.println(newAnimal);
+            return new ModelAndView(model, "success.hbs");
+        } , new HandlebarsTemplateEngine());
+
+        // String ranger,String location, int animal_id
+    }
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,22 @@
-public class DatabaseRule {
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "isaac", "kaptoge");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteSightingQuery = "DELETE FROM sightings *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingQuery).executeUpdate();
+        }
+    }
+
 }
