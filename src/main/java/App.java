8d3c58c6e80a061461756f9ed21b5bc52d241847
Isaac23-K <ikaptoge@yahoo.com>
2021-06-15 17:34:21 +0300

    added css styles to the code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,44 +18,42 @@ public class App {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
-
-
         //get sightings
         get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String,Object>();
+            Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "homepage.hbs");
         }, new HandlebarsTemplateEngine());
 
-//        get("/success", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            List<Animal> animals = Animal.all();
-//            model.put("animals",animals);
-//            return new ModelAndView(model, "templates/homepage.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//
-//        //String name, String endangered, String health , String age
-//        post("/show", (request, response) -> {
-//            Map<String,Object> model = new HashMap<>();
-//            List<Animal> animals = Animal.all();
-//            String name = request.queryParams("rangerName");
-//            String endangered = request.queryParams("animal");
-//            String health = request.queryParams("health");
-//            String age = request.queryParams("age");
-//            Animal animal = new Animal(name, endangered, health, age);
-//            animal.save();
-//            model.put("animals",animals);
-////            Sighting newSighting = new Sighting(ranger, location, newAnimal.getId());
-////            model.put("ranger", newSighting.getRanger());
-////            model.put("location",newSighting.getLocation());
-////            newSighting.save();
-////            model.put("animals",Animal.all());
-////            model.put("sighting",Sighting.all());
-////            model.put("AnimalClass",Animal.class);
-////            System.out.println(newAnimal);
-//            return new ModelAndView(model, "templates/success.hbs");
-//        } , new HandlebarsTemplateEngine());
-//
-//        // String ranger,String location, int animal_id
+        get("/success", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            model.put("animals", animals);
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //String name, String endangered, String health , String age
+        post("/show", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            String name = request.queryParams("rangerName");
+            String endangered = request.queryParams("animal");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            Animal animal = new Animal(name, endangered, health, age);
+            animal.save();
+            model.put("animals", animals);
+//            Sighting newSighting = new Sighting(ranger, location, newAnimal.getId());
+//            model.put("ranger", newSighting.getRanger());
+//            model.put("location",newSighting.getLocation());
+//            newSighting.save();
+//            model.put("animals",Animal.all());
+//            model.put("sighting",Sighting.all());
+//            model.put("AnimalClass",Animal.class);
+//            System.out.println(newAnimal);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // String ranger,String location, int animal_id
     }
 }
